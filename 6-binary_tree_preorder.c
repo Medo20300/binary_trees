c4d9b47ba30d4b9ab638c75ea6b5b214abd92e11
Medo20300 <medhatconcrete@gmail.com>
2024-02-28 12:47:41 -0800

    2050

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,15 +1,18 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_preorder - go through a binary tree using pre-order traversal
- * @tree: pointer to the root node of the tree
- * @func: the function that use to print the value of the node
-*/
+ * binary_tree_preorder - recursively proceeds through a `binary_tree_t`
+ * binary tree using pre-order traversal
+ * @tree: root node of tree to traverse
+ * @func: pointer to function to call for each node; value in the node is
+ * passed as a parameter to `func`
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func)
-	{
-		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
-	}
+	if (!tree || !func)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
