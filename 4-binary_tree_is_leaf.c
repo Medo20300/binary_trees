5528d600528d9a9e5ac7b737c346b1db93832979
mahmoudramzy1 <97908178+mahmoudramzy1@users.noreply.github.com>
2024-02-28 17:08:11 +0200

    Update 4-binary_tree_is_leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,12 +5,12 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return 0;
+	if (node == NULL)
+		return 0;
+	
+	if (node->left || node->right)
+		return 0;
+	else
+		return 1;
 
-    if (node->left || node->right)
-        return 0;
-    else
-        return 1;
-    
 }
