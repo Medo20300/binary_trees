359330c289d29d08e10d2044581d93ef31cc9238
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-02-28 07:09:47 -0800

    p

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,11 +6,11 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return 0;
-	
+		return (0);
+
 	if (node->left || node->right)
-		return 0;
+		return (0);
 	else
-		return 1;
+		return (1);
 
 }
