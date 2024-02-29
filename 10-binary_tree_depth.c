8d393afd4b778d8cfa4d11ffe881e79102d13875
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-02-29 04:50:22 -0800

    p

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - calculate the depth of the passed node
+ * @tree: the node
+ * Return: the depth of the passed node or NULL if the tree is null
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    int i;
+    for (i = 0; tree->parent != NULL; i++)
+    {
+        tree = tree->parent;
+    }
+    return (i);
+}
