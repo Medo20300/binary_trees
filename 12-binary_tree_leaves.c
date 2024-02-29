7c9f57a6a5b58063381509441fb8c746d03ffe08
mahmoudramzy1 <97908178+mahmoudramzy1@users.noreply.github.com>
2024-02-29 16:45:28 +0200

    Update 12-binary_tree_leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -6,23 +6,21 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-  size_t size = 0, l = 0, r = 0;
-
-  if (tree == NULL)
-  {
-    return (0);
-  }
-  else
-  {
-    if (tree->left == NULL && tree->right == NULL)
-    {
-      return (1);
-    }
-    l = binary_tree_leaves(tree->left);
-    r = binary_tree_leaves(tree->right);
-    size = l + r;
-    
-  }
-  return (size);
+	size_t size = 0, l = 0, r = 0;
+	
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree->left == NULL && tree->right == NULL)
+	{
+	return (1);
+	}
+		l = binary_tree_leaves(tree->left);
+		r = binary_tree_leaves(tree->right);
+		size = l + r;
+	}
+	return (size);
 }
-
