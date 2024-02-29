46ea8ab95d1ca96503d36aeb9566749463afee46
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-02-29 07:02:58 -0800

    p

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,7 +6,7 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int l = 0, l = 0;
+	int l = 0, r = 0;
 
 	if (tree == NULL)
 	{
@@ -16,8 +16,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	{
 		if (tree)
 		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 		}
 		return (l - r);
 	}
